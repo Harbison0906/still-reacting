abbab9f7f6cb06531918b8cc747f6694e8e5821e
Seth Harbison <sethharbison@Seths-MacBook-Pro.local>
2021-09-01 12:54:07 -0500

    added button to toggle state of 'loaded'; added useEffect to toggle state after 3 seconds

--- still-reacting-to-react/src/App.js
+++ still-reacting-to-react/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Greeter from './components/Greeter';
 
 
@@ -7,24 +7,36 @@ const App = () => {
   const [username, setUsername] = useState('')
   const [loaded, setLoaded] = useState(false)
 
+  const handleToggle = () => {
+    setLoaded(!loaded);
+  }
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLoaded(!loaded)
+    }, 3000)
+  })
+
   if (loaded === false) {
     return (
-      <h1>Website loading...</h1>
+      <>
+        <h1>Website loading...</h1>
+        <button onClick={handleToggle}>Toggle State</button>
+      </> 
     )
-  }
-  return (
-    <>
-      <h1 className="text-center">Greetings!</h1>
-
-      <Greeter name="Seth" phrase="Yo" />
-      <Greeter name="Katelyn" phrase="Hey there" />
-      <Greeter name="Ben" phrase="Good to see you" />
-
-      <span className="mr-1">Username</span><input value={username} onChange={(e => setUsername(e.target.value))} />
-      <p>You are logging in as: {username}</p>
-    </>
-  )
-
+  } 
+    return (
+      <>
+        <h1 className="text-center">Greetings!</h1>
+  
+        <Greeter name="Seth" phrase="Yo" />
+        <Greeter name="Katelyn" phrase="Hey there" />
+        <Greeter name="Ben" phrase="Good to see you" />
+  
+        <span className="mr-1">Username</span><input value={username} onChange={(e => setUsername(e.target.value))} />
+        <p>You are logging in as: {username}</p>
+      </>
+    )
 }
 
 export default App;
