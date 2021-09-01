243643e2938d9395076f718ba756c952b00dba88
Seth Harbison <sethharbison@Seths-MacBook-Pro.local>
2021-08-31 22:20:32 -0500

    added Greeter component with 'name' and 'phrase' as props; added controlled react input

--- still-reacting-to-react/src/App.js
+++ still-reacting-to-react/src/App.js
@@ -1,9 +1,19 @@
-import React from 'react';
+import React, {useState} from 'react';
+import Greeter from './components/Greeter';
+
 
 const App = () => {
+
+const [username, setUsername] = useState('')
+
   return (
 <>
-    <h1>Hello there</h1>
+    <h1 className="text-center">Greetings!</h1>
+    <Greeter name="Seth" phrase="Yo"/>
+    <Greeter name="Katelyn" phrase="Hey there"/>
+    <Greeter name="Ben" phrase="Good to see you"/>
+
+    <span className="mr-1">Username</span><input value={username} onChange={(e => setUsername(e.target.value))}/>
 </>
   )
 }
---
+++ still-reacting-to-react/src/components/Greeter.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Greeter = props => {
+  return (
+    <>
+      <h1 className="shadow-sm m-4 border p-2">{props.phrase}, {props.name}</h1>
+    </>
+  )
+}
+
+export default Greeter;
