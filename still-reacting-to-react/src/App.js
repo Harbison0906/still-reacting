18ae06925e34dae84efc2c8dfb8de6275a9750d9
Seth Harbison <sethharbison@Seths-MacBook-Pro.local>
2021-08-31 22:52:53 -0500

    added conditional rendering to show 'Website loading' if certain state is false

--- still-reacting-to-react/src/App.js
+++ still-reacting-to-react/src/App.js
@@ -1,21 +1,30 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import Greeter from './components/Greeter';
 
 
 const App = () => {
 
-const [username, setUsername] = useState('')
+  const [username, setUsername] = useState('')
+  const [loaded, setLoaded] = useState(false)
 
+  if (loaded === false) {
+    return (
+      <h1>Website loading...</h1>
+    )
+  }
   return (
-<>
-    <h1 className="text-center">Greetings!</h1>
-    <Greeter name="Seth" phrase="Yo"/>
-    <Greeter name="Katelyn" phrase="Hey there"/>
-    <Greeter name="Ben" phrase="Good to see you"/>
+    <>
+      <h1 className="text-center">Greetings!</h1>
 
-    <span className="mr-1">Username</span><input value={username} onChange={(e => setUsername(e.target.value))}/>
-</>
+      <Greeter name="Seth" phrase="Yo" />
+      <Greeter name="Katelyn" phrase="Hey there" />
+      <Greeter name="Ben" phrase="Good to see you" />
+
+      <span className="mr-1">Username</span><input value={username} onChange={(e => setUsername(e.target.value))} />
+      <p>You are logging in as: {username}</p>
+    </>
   )
+
 }
 
 export default App;
